353e9fb034da6ddee59cebbf32b5c690df8fadee
MrLately <94589563+MrLately@users.noreply.github.com>
2023-01-31 19:20:50 -0500

    Update LED_control.py

--- bas/LED_control.py
+++ bas/LED_control.py
@@ -22,21 +22,36 @@ pixels = neopixel.NeoPixel(
 color = (0, 255, 0)
 slide_wait = 0.05
 blink_wait = 0.5
+block_size = 2
 
 def slide_pattern():
-    for i in range(num_pixels):
+    for i in range(0, num_pixels, block_size):
         pixels.fill((0, 0, 0))
-        pixels[i] = color
+        for j in range(block_size):
+            if i + j < num_pixels:
+                pixels[i + j] = color
         pixels.show()
         time.sleep(slide_wait)
-    for i in range(num_pixels-1, -1, -1):
+    for i in range(num_pixels - block_size, -block_size, -block_size):
         pixels.fill((0, 0, 0))
-        pixels[i] = color
+        for j in range(block_size):
+            if i + j < num_pixels:
+                pixels[i + j] = color
         pixels.show()
         time.sleep(slide_wait)
 
 def blink_pattern():
     while True:
+        pixels.fill((0, 0, 0))
+        for i in range(block_size):
+            pixels[i] = color
+        pixels.show()
+        time.sleep(blink_wait)
+        pixels.fill((0, 0, 0))
+        for i in range(num_pixels - block_size, num_pixels):
+            pixels[i] = color
+        pixels.show()
+        time.sleep(blink_wait)
         pixels.fill((0, 0, 0))
         pixels[0] = color
         pixels.show()
