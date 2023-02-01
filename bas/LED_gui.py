ceac3f7e55eea7a0c38cbad5cda7492830fa441e
MrLately <94589563+MrLately@users.noreply.github.com>
2023-01-31 19:20:26 -0500

    Update LED_gui.py

--- bas/LED_gui.py
+++ bas/LED_gui.py
@@ -9,7 +9,7 @@ def toggle_pattern():
         btn_start_stop.config(text="Start")
         is_running = False
     else:
-        LED_control.start(pattern_var.get())
+        LED_control.start(pattern_var.get(), block_size_var.get())
         btn_start_stop.config(text="Stop")
         is_running = True
 
@@ -18,6 +18,9 @@ root.title("LED Control")
 
 pattern_var = tk.StringVar()
 pattern_var.set("slide")
+
+block_size_var = tk.IntVar()
+block_size_var.set(1)
 
 lbl_pattern = ttk.Label(root, text="Pattern:")
 lbl_pattern.grid(row=0, column=0, sticky="W")
@@ -28,10 +31,15 @@ rb_slide.grid(row=1, column=0, sticky="W")
 rb_blink = ttk.Radiobutton(root, text="Blink", variable=pattern_var, value="blink")
 rb_blink.grid(row=2, column=0, sticky="W")
 
+lbl_block_size = ttk.Label(root, text="Block size:")
+lbl_block_size.grid(row=3, column=0, sticky="W")
+
+spin_block_size = tk.Spinbox(root, from_=1, to=60, textvariable=block_size_var)
+spin_block_size.grid(row=4, column=0, sticky="W")
+
 btn_start_stop = ttk.Button(root, text="Start", command=toggle_pattern)
-btn_start_stop.grid(row=3, column=0)
+btn_start_stop.grid(row=5, column=0)
 
 is_running = False
 
 root.mainloop()
-
